74ae969910f08fb15eff98477c0feac6d387b152
tranquangsonyuuta7 <tranquangsonyuuta7@gmail.com>
2021-10-19 01:17:13 +0700

    Setup sequelize and connect to database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const database = require('./database/models/index');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+database.testConnection();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
